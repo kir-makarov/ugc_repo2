f81808321f08f7f55807f4887f84686994230777
Kirill Makarov <k.makarov163@yandex.ru>
2022-07-10 00:58:13 +0400

    Добавил загрузку тестовых данных в монго и постгрес

---
+++ benchmark_db/pg_test.py
@@ -0,0 +1,29 @@
+import time
+import psycopg2
+
+conn = psycopg2.connect("host=localhost dbname=movies_database user=app password=123qwe")
+cur = conn.cursor()
+cur.execute("CREATE TABLE IF NOT EXISTS test_table (id INT, data TEXT)")
+conn.commit()
+
+
+def saving_data_test():
+    start = time.time()
+    for i in range(3000000):
+        cur.execute("INSERT INTO test_table (id, data) VALUES (%(id)s,%(data)s)", {"id": i, "data": f'somedata_{i}'})
+    conn.commit()
+    return time.time() - start
+
+
+def find_data_test():
+    start = time.time()
+    cur.execute(f"SELECT id, data FROM test_table WHERE id < 10000")
+    data = cur.fetchall()
+    for _ in data:
+        continue
+    return time.time() - start
+
+
+if __name__ == '__main__':
+    print(saving_data_test())
+    print(find_data_test())
